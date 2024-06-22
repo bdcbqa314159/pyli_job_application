2683cf8a8ce54a5f55c9f647d35d4613f135a741
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2024-06-22 17:14:54 +0100

    listing of jobs

--- main.py
+++ main.py
@@ -6,6 +6,24 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 
+class LinkedInUrl:
+    def __init__(self):
+        load_dotenv()
+        self.url = os.getenv("URL_LINKEDIN")
+    
+    def __call__(self):
+        return self.url
+    
+class SetupDriver():
+    def __init__(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_experimental_option("detach", True)
+        self._driver = webdriver.Chrome(options=chrome_options)
+
+    @property
+    def driver(self):
+        return self._driver
+
 class EnvLoader:
     def __init__(self):
         load_dotenv()
@@ -16,16 +34,6 @@ class EnvLoader:
     def __call__(self):
         return self.account_email, self.account_password, self.phone
 
-class SetupDriver():
-    def __init__(self):
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_experimental_option("detach", True)
-        self._driver = webdriver.Chrome(options=chrome_options)
-
-    @property
-    def driver(self):
-        return self._driver
-    
 class AbortApplication:
     def __init__(self, driver):
         self._driver = driver
@@ -127,49 +135,76 @@ class ApplicationFilling:
         input("Press Enter when you have solved the Captcha")
 
         time.sleep(5)
-        all_listings = self.driver.find_elements(by=By.CSS_SELECTOR, value=".job-card-container--clickable")
+        
+        apply_button = driver.find_element(by=By.LINK_TEXT, value="Jobs")
+        apply_button.click()
+        
+        number = 100
+        while True:
 
-        for listing in all_listings:
-            print("Opening Listing")
-            listing.click()
-            time.sleep(2)
+            # xpath = f'//*[@id="jobs-search-box-keyword-id-ember{number}"]'
+            id_job = f"jobs-search-box-keyword-id-ember{number}"
+            id_loc = f"jobs-search-box-location-id-ember{number}"
             try:
-                # Click Apply Button
-                apply_button = self.driver.find_element(by=By.CSS_SELECTOR, value=".jobs-s-apply button")
-                apply_button.click()
-
-                # Insert Phone Number
-                # Find an <input> element where the id contains phoneNumber
-                time.sleep(5)
-                phone = self.driver.find_element(by=By.CSS_SELECTOR, value="input[id*=phoneNumber]")
-                if phone.text == "":
-                    phone.send_keys(phone_number)
-
-                # Check the Submit Button
-                submit_button = self.driver.find_element(by=By.CSS_SELECTOR, value="footer button")
-                if submit_button.get_attribute("data-control-name") == "continue_unify":
-                    abortApplication()
-                    print("Complex application, skipped.")
-                    continue
-                else:
-                    # Click Submit Button
-                    print("Submitting job application")
-                    submit_button.click()
-
-                time.sleep(2)
-                # Click Close Button
-                close_button = self.driver.find_element(by=By.CLASS_NAME, value="artdeco-modal__dismiss")
-                close_button.click()
-
-            except NoSuchElementException:
-                abortApplication()
-                print("No application button, skipped.")
+                job_title_field = driver.find_element(by=By.ID, value=id_job)
+                job_location_field = driver.find_element(by=By.ID, value=id_loc)
+                job_title_field.send_keys("Design Graphique / Directeur Artisitique")
+                job_location_field.send_keys("Paris Toronto")
+                job_title_field.send_keys(Keys.ENTER)
+            except:
+                number+=1
                 continue
+            else:
+                print(f"Job Title and Location Entered number:{number}")
+                break
+
+        # job_title_field = driver.find_element(by=By.XPATH, value='//*[@id="jobs-search-box-keyword-id-ember326"]')
+        # job_title_field.send_keys("Design Graphique")
+        # job_title_field.send_keys(Keys.ENTER)
+        
+        # "Search by title, skill, or company"
+        # for listing in all_listings:
+        #     print("Opening Listing")
+        #     listing.click()
+        #     time.sleep(2)
+        #     try:
+        #         # Click Apply Button
+        #         apply_button = self.driver.find_element(by=By.CSS_SELECTOR, value=".jobs-s-apply button")
+        #         apply_button.click()
+
+        #         # Insert Phone Number
+        #         # Find an <input> element where the id contains phoneNumber
+        #         time.sleep(5)
+        #         phone = self.driver.find_element(by=By.CSS_SELECTOR, value="input[id*=phoneNumber]")
+        #         if phone.text == "":
+        #             phone.send_keys(phone_number)
+
+        #         # Check the Submit Button
+        #         submit_button = self.driver.find_element(by=By.CSS_SELECTOR, value="footer button")
+        #         if submit_button.get_attribute("data-control-name") == "continue_unify":
+        #             abortApplication()
+        #             print("Complex application, skipped.")
+        #             continue
+        #         else:
+        #             # Click Submit Button
+        #             print("Submitting job application")
+        #             submit_button.click()
+
+        #         time.sleep(2)
+        #         # Click Close Button
+        #         close_button = self.driver.find_element(by=By.CLASS_NAME, value="artdeco-modal__dismiss")
+        #         close_button.click()
+
+        #     except NoSuchElementException:
+        #         abortApplication()
+        #         print("No application button, skipped.")
+        #         continue
 
         return
                 
 if __name__ == "__main__":
-    url = "the_url_to_linkedin"
+    linkedin_url = LinkedInUrl()
+    url = linkedin_url()
     mySetup = SetupDriver()
     driver = mySetup.driver
 
@@ -183,7 +218,7 @@ if __name__ == "__main__":
     cookiesdenial()
     signIn()
     applicationFilling()
-    killer()
+    # killer()
 
     
     
